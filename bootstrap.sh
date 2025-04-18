de44707efe83d71c7da9df11633bf00b1ddd4a7a
Sam Fleming <me@sam-fleming.co.uk>
2025-04-18 09:45:39 +0100

    feat: add nightfox tmux and base16 themes

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,9 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "base16-themes" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	cp -r base16-themes/ ~/.config/base16-shell/scripts/
 	source ~/.bash_profile;
 }
 
