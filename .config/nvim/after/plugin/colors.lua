11e9930519f3aac9e951ccf37d72748534631aca
Sam Fleming <me@sam-fleming.co.uk>
2025-04-18 10:01:00 +0100

    fix: use colorscheme from base16 theme env var

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "tokyonight-moon"
+	color = color or os.getenv("BASE16_THEME") or "tokyonight-moon"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
